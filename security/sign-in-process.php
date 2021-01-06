53fe3157c01b693991f8bdd9211087fbef710dd5
Developer Aljannah <76476702+developeraljannah@users.noreply.github.com>
2021-01-06 18:25:11 +0700

    Update sign-in-process.php

--- security/sign-in-process.php
+++ security/sign-in-process.php
@@ -10,9 +10,13 @@
     }else {
         $user = mysqli_fetch_array($query);
         $nama_mitra = $user['nama_mitra'];
+        $email = $user['email'];
+        $kontak = $user['kontak'];
         if($user['is_verified']==1){
                 session_start();
                 $_SESSION['nama_mitra'] = $nama_mitra;
+                $_SESSION['email'] = $email;
+                $_SESSION['kontak'] = $kontak;
                 $_SESSION['kondisi'] = "login";
                 echo "<script>alert('Selamat, anda berhasil login');window.location='../data_mitra/dashboard'</script>";
             }else {
