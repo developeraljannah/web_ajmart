b3c47a0b8ca579f7eb992603f9251e5c8e61c308
Developer Aljannah <76476702+developeraljannah@users.noreply.github.com>
2021-01-06 07:28:17 +0700

    Delete logout.php

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-// mengaktifkan session
-session_start();
-
-// menghapus semua session
-session_destroy();
-
-// mengalihkan halaman sambil mengirim pesan logout
-echo "<script>alert('Anda telah logout dari aplikasi');window.location='../sign-in'</script>";
