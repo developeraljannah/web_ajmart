e625dd97dc34f814cd0e2070e6905b144b0784f5
Developer Aljannah <76476702+developeraljannah@users.noreply.github.com>
2021-01-06 07:31:13 +0700

    Add files via upload

---
+++ data_mitra/include/footer.php
@@ -0,0 +1,9 @@
+<!-- Footer -->
+<footer class="sticky-footer bg-white">
+<div class="container my-auto">
+    <div class="copyright my-auto">
+    <span>Copyright &copy; AJ MArt 2020</span>
+    </div>
+</div>
+</footer>
+<!-- End of Footer -->
---
+++ data_mitra/include/sidebar-in.php
@@ -0,0 +1,59 @@
+<!-- Sidebar -->
+<ul class="navbar-nav bg-gradient-info sidebar sidebar-dark accordion" id="accordionSidebar">
+
+<!-- Sidebar - Brand -->
+<a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+  <div class="sidebar-brand-icon">
+    <i class="fas fa-home"></i>
+  </div>
+  <div class="sidebar-brand-text mx-3">AJ Mart</div>
+</a>
+
+<!-- Divider -->
+<hr class="sidebar-divider my-0">
+
+<!-- Nav Item - Dashboard -->
+<li class="nav-item">
+  <a class="nav-link" href="../dashboard">
+    <i class="fas fa-fw fa-home"></i>
+    <span>Dashboard</span></a>
+</li>
+
+<!-- Nav Item - Pelapak -->
+<li class="nav-item">
+  <a class="nav-link" href="../mitra/lihat_mitra.php">
+    <i class="fas fa-fw fa-users"></i>
+    <span>Data Mitra</span></a>
+</li>
+
+<!-- Nav Item - Toko -->
+<li class="nav-item">
+  <a class="nav-link" href="#">
+    <i class="fas fa-fw fa-store"></i>
+    <span>Data Toko</span></a>
+</li>
+
+<!-- Nav Item - Produk -->
+<li class="nav-item">
+  <a class="nav-link" href="#">
+    <i class="fas fa-fw fa-box"></i>
+    <span>Data Produk</span></a>
+</li>
+
+<!-- Nav Item - Komentar -->
+<li class="nav-item">
+  <a class="nav-link" href="#">
+    <i class="fas fa-fw fa-comments"></i>
+    <span>Data Komentar</span></a>
+</li>
+
+<!-- Divider -->
+<hr class="sidebar-divider d-none d-md-block">
+
+<!-- Sidebar Toggler (Sidebar) -->
+<div class="text-center d-none d-md-inline">
+  <button class="rounded-circle border-0" id="sidebarToggle"></button>
+</div>
+
+</ul>
+<!-- End of Sidebar -->
